d0c2dc281db466d01be4ae34b6953f3a8c4234d0
lan-k <Lan.Kelly@unisa.edu.au>
2023-08-10 14:44:37 +0930

    initial commit

---
+++ psoriasis PBS codes.R
@@ -0,0 +1,10 @@
+##find PBS code for psoriasis medicines
+
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+med_list <- c("ADALIMUMAB","ETANERCEPT","GUSELKUMAB","INFLIXIMAB",
+              "IXEKIZUMAB","RISANKIZUMAB","SECUKINUMAB","TILDRAKIZUMAB","USTEKINUMAB")
+
+
